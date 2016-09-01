7a3e416ebc6753eaca0a26dbc41993ae1d848d73
Cyril Lequeux <l.cyril@gmail.com>
2016-09-01 14:00:12 +0200

    fix(router): bug fixing

--- index.js
+++ index.js
@@ -17,50 +17,54 @@ router.getRoutes = function getRoutes() {
 //match(routeName,req,this).then
 //match(routeName,req,cb,[this])
 router.match = function matchRoute(routeName, req, done, _this) {
-    var ret = null, donePromise = function (res) {
-        setImmediate(function () {
-            res instanceof Error ? ret.reject(res) : ret.resolve(res);
-        });
-    };
-    if (arguments.length == 2) //match(routeName,req).then
-        ret = when.defer(), done = donePromise, _this = null;
-    else if (arguments.length == 3 && !_.isFunction(arguments[2])) //match(routeName,req,this).then
-        ret = when.defer(), _this = done, done = donePromise;
-    var route = _.isString(routeName) ? routes[routeName] : routeName;
-    if (!_.isArray(route) || !route.length) {
-        done(new ERR("invalidRoute", {r: routeName}));
-        return;
-    }
-    route = _.clone(route);
+    var def = null,
+        prom = null,
+        donePromise = function (res) {
+            setImmediate(function () {
+                res instanceof Error ? def.reject(res) : def.resolve(res);
+            });
+        },
+        route = _.isString(routeName) ? routes[routeName] : routeName,
+        fn;
 
-    var fn = route.shift();
-    var next = function (res, controller) {
-        if (res instanceof Error)
-            done(new ERR().use(res));
-        else if (!route.length)
-            done(res);
-        else {
-            try {
-                if (controller != void 0) { //Jump to a specific controller
-                    while (controller !== fn && route.length)
+    if (arguments.length == 2) //match(routeName,req).then
+        def = when.defer(), prom = def.promise, done = donePromise, _this = null;
+    else if (arguments.length == 3 && !_.isFunction(arguments[2])) //match(routeName,req,this).then
+        def = when.defer(), prom = def.promise, _this = done, done = donePromise;
+
+    if (!_.isArray(route) || !route.length)
+        done(new ERR("invalidRoute", {r: routeName}));
+    else {
+        route = _.clone(route);
+        fn = route.shift();
+        var next = function (res, controller) {
+            if (res instanceof Error)
+                done(new ERR().use(res));
+            else if (!route.length)
+                done(res);
+            else {
+                try {
+                    if (controller != void 0) { //Jump to a specific controller
+                        while (controller !== fn && route.length)
+                            fn = route.shift();
+                    }
+                    else
                         fn = route.shift();
+                    fn.call(_this || fn, req, res, next, done);
+                } catch (e) {
+                    e = new ERR({req: req}).use(e);
+                    next(e);
                 }
-                else
-                    fn = route.shift();
-                fn.call(_this || fn, req, res, next, done);
-            } catch (e) {
-                e = new ERR({req: req}).use(e);
-                tracer.fatal(e, "Unexpected Error");
-                next(e);
             }
+        };
+        try {
+            fn.call(_this || fn, req, req, next, done);
+        } catch (e) {
+            e = new ERR({req: req}).use(e);
+            next(e);
         }
-    };
-    try {
-        fn.call(_this || fn, req, null, next, done);
-    } catch (e) {
-        e = new ERR({req: req}).use(e);
-        next(e);
     }
+    return prom;
 };
 
 module.exports = router;
